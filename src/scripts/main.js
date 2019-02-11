791bbbd8ff948362e812e38ce454b1ac5cd5fb21
t-john93 <tjohnson1293@gmail.com>
2019-02-11 14:14:04 -0800

    restructured directory tree

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+
+
+var ship = new Ship( 0, 0, 1000, 100, 1000, "basic", false );
+
+
+// when DOM loaded, call this
+window.onload = function () {
+
+    updateHeading();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
